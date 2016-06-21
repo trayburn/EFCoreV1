34cf3fdbdd77d26c0ce823f216f91de9e8f884ca
Tim Rayburn <tim@timrayburn.net>
2016-06-21 16:13:04 -0500

    Working Demo

---
+++ Migrations/20160621163531_MyFirstMigration.Designer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ConsoleApplication;
+
+namespace efcore1v2.Migrations
+{
+    [DbContext(typeof(BloggingContext))]
+    [Migration("20160621163531_MyFirstMigration")]
+    partial class MyFirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rc2-20896");
+
+            modelBuilder.Entity("ConsoleApplication.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blogs");
+                });
+
+            modelBuilder.Entity("ConsoleApplication.Post", b =>
+                {
+                    b.Property<int>("PostId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BlogId");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("PostId");
+
+                    b.HasIndex("BlogId");
+
+                    b.ToTable("Posts");
+                });
+
+            modelBuilder.Entity("ConsoleApplication.Post", b =>
+                {
+                    b.HasOne("ConsoleApplication.Blog")
+                        .WithMany()
+                        .HasForeignKey("BlogId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using static System.Console;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static readonly int NumberOfBlogs = 5;
+        public static readonly int NumberOfPostsPerBlog = 10;
+        public static void Main(string[] args)
+        {
+            var context = new BloggingContext();
+
+            WriteLine("---------------------------------------------------");
+            WriteLine("Entity frameworks Core 1.0 Demos");
+            WriteLine("---------------------------------------------------");
+            WriteLine($"{context.Posts.CountAsync().Result} Posts found.");
+            WriteLine($"{context.Blogs.CountAsync().Result} Blogs found.");
+
+            WriteLine("Removing All DB entries.");
+            foreach(var post in context.Posts)
+            {
+                context.Posts.Remove(post);
+            }
+            foreach(var blog in context.Blogs)
+            {
+                context.Blogs.Remove(blog);
+            }
+            context.SaveChanges();
+
+            WriteLine($"{context.Posts.CountAsync().Result} Posts found.");
+            WriteLine($"{context.Blogs.CountAsync().Result} Blogs found.");
+            WriteLine("---------------------------------------------------");
+
+            WriteLine("Creating Data...");
+            for(int blogIndex = 0; blogIndex < NumberOfBlogs; blogIndex++)
+            {
+                var blog = new Blog() 
+                { 
+                    Url = $"http://mysite/{blogIndex}/blog",
+                    Name = $"Blog #{blogIndex + 1}"
+                };
+                context.Blogs.Add(blog);
+
+                for(int postIndex = 0; postIndex < NumberOfPostsPerBlog; postIndex++)
+                {
+                    var post = new Post
+                    {
+                        Title = $"Post #{postIndex + 1} Blog #{blogIndex + 1}",
+                        Content = "Lorem ipsum delorum est",
+                        Blog = blog 
+                    };
+                    context.Posts.Add(post);
+                }
+            }
+            context.SaveChanges();
+            WriteLine("Created Data.");
+
+            WriteLine("---------------------------------------------------");
+            WriteLine($"{context.Posts.CountAsync().Result} Posts found.");
+            WriteLine($"{context.Blogs.CountAsync().Result} Blogs found.");
+            WriteLine("---------------------------------------------------");
+            WriteLine("Complete.");
+        }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // optionsBuilder.UseInMemoryDatabase();
+            optionsBuilder.UseSqlite("Filename=./blog.db");
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
